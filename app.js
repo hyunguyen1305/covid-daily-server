eac2761346965f0126c0482c4250561c9cd2800f
hyunguyen1305 <huy.nguyenngoc1305@gmail.com>
2020-05-18 14:47:18 +0700

    Update CORS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const port = process.env.PORT || 1235;
 const app = express();
@@ -6,17 +7,14 @@ const app = express();
 const dataTableRoutes = require("./routes/data-table");
 const dataNewsRoutes = require("./routes/data-news");
 
-
-app.get('/', function (req, res) {
-    res.send("<h1>API for Covid Daily</h1>");
+app.use(cors());
+app.get("/", function (req, res) {
+  res.send("<h1>API for Covid Daily</h1>");
 });
 
-app.use("/api", dataTableRoutes)
-app.use("/api", dataNewsRoutes)
-
-
-
+app.use("/api", dataTableRoutes);
+app.use("/api", dataNewsRoutes);
 
 app.listen(port, function () {
-    console.log(`App listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
